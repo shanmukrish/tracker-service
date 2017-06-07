164de7c2366ba688b7efec50f0815691fa4fc14c
shanmukrish <shanmuganathankrishnan@live.com>
2017-06-07 09:15:18 +0530

    Added Model for user registration and controller for user registration

---
+++ src/main/java/com/goihram/haj/trackerservice/controller/UserRegistrationController.java
@@ -0,0 +1,18 @@
+package com.goihram.haj.trackerservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserRegistrationController {
+
+	
+	@RequestMapping("register")
+	public void registerUser()
+	{
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/goihram/haj/trackerservice/model/UserInformation.java
@@ -0,0 +1,40 @@
+package com.goihram.haj.trackerservice.model;
+
+public class UserInformation {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private long mobileNumber;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
+	
+	
+	
+	
+}
