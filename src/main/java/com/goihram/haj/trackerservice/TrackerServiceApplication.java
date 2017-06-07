0dfca8e338cdb8d3f7b1a98356c9bc7076c12d2e
shanmukrish <shanmuganathankrishnan@live.com>
2017-06-07 08:42:16 +0530

    Initial Checkin for project creation

---
+++ src/main/java/com/goihram/haj/trackerservice/TrackerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.goihram.haj.trackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/goihram/haj/trackerservice/TrackerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goihram.haj.trackerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrackerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
